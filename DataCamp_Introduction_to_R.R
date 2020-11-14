44e3a2b59003fe3f3630974550522a420cbb6b0b
Anthony Long <anthony.long618@gmail.com>
2020-11-14 13:01:45 -0500

    Testing

---
+++ DataCamp_Introduction_to_R.R
@@ -0,0 +1,20 @@
+# Title     : Data Camp Intro to R
+# Objective : Complete Lessons and Upload Repo to Github
+# Created by: Anthony S. Long
+# Created on: 11/14/2020
+
+
+#Change my_numeric to be 42
+my_numeric <-42
+
+#Change my_character to be universe
+my_character <- "universe"
+
+#Change my_logical to be FALSE
+my_logical <- FALSE
+
+#Creating Vectors
+#Testing to see if Github Commit + Push works from PyCharm
+
+
+
