2c5d6004b4c508c8633582a4db5fd56d692b9b6a
Anthony Long <anthony.long618@gmail.com>
2020-11-14 17:37:24 -0500

    (PyCharm) R DataCamp Lesson Update, Added Lessons on Vectors and Variable Assignment

--- DataCamp_Introduction_to_R.R
+++ DataCamp_Introduction_to_R.R
@@ -14,16 +14,22 @@ my_character <- "universe"
 my_logical <- FALSE
 
 #Creating Vectors
+  ##Vector for Poker winnings from Monday to Friday
+poker_vector <- c(140, -50, 20, -120, 240)
+
+  ##Roulette winnings from Mon - Fri
+roulette_vector <- c(-24, -50, 100, -350)
+
+  ##Create Days Vector to not have to type days out
+days_vector <- c("Monday", "Tuesday", "Wednesday","Thursday", "Friday")
+
+# Assign the names of the day to roulette_vector and poker_vector
+names(poker_vector) <- days_vector
+names(roulette_vector) <- days_vector
 
 
 
 
-
-
-
-
-
-#Testing PyCharm Push
 
 
 
