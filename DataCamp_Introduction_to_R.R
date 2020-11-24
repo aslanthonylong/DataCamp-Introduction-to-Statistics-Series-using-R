a5ec98657572c04965045f7d3385c233a2bb211f
Anthony Long <anthony.long618@gmail.com>
2020-11-23 21:05:59 -0500

    Chapters 1-3 Completed; Chapter 4 Completed; Working on Chapter 5

--- DataCamp_Introduction_to_R.R
+++ DataCamp_Introduction_to_R.R
@@ -205,6 +205,70 @@ da2 > da5
 
 ### DATA FRAMES
 
+# When doing a market research survey, you'll have the following questions:
+  # Yes/No Questions - Logical
+  # Value/Numeric ("How old are you?")
+  # Character ("What is your opinion on this product?")
+
+# Data Frame - has the variables of data set as columns
+  # Has the observations as rows
+# Working "mtcars" dataset
+
+# Useful commands/functions
+  # head() - shows the first observations of data set
+  # tail() - shows the last observations in data set
+# Call head() on mtcars
+head(mtcars)
+# Structure - str() function
+str(mtcars)
+# Create own data frame
+name <- c("Mercury", "Venus", "Earth",
+          "Mars", "Jupiter", "Saturn",
+          "Uranus", "Neptune")
+type <- c("Terrestrial planet",
+          "Terrestrial planet",
+          "Terrestrial planet",
+          "Terrestrial planet", "Gas giant",
+          "Gas giant", "Gas giant", "Gas giant")
+diameter <- c(0.382, 0.949, 1, 0.532,
+              11.209, 9.449, 4.007, 3.883)
+rotation <- c(58.64, -243.02, 1, 1.03,
+              0.41, 0.43, -0.72, 0.67)
+rings <- c(FALSE, FALSE, FALSE, FALSE, TRUE, TRUE, TRUE, TRUE)
+# use data.frame() to include select vectors into your data set
+planets_df <- data.frame(name,type,diameter,rotation,rings)
+
+str(planets_df)
+
+# Selection of Data elements - It's similar to selection of martices!
+  #Use [row,column] same as before
+# Print out diameter of Mercury (row 1, column 3)
+planets_df[1,3]
+
+# Print out data for Mars (entire fourth row)
+planets_df[4,]
+## FYI : A possible disadvantage of this approach is that you have to know (or look up) the column number of the
+  # variable which gets hard if you have a lot of variables. It is often easier to just make use of the variable name
+    # Example to find variable "type": planets_df [1:3, "type"]
+
+# Practice : selet the first 5 values of the diameter column
+planets_df[1:5,"diameter"]
+#Only planets with rings
+# Instead of planets_df[,3] or planets_df[,"diameter"] we can use shortcut
+  ## shortcut planets_df$diameter - only works if columns have names
+rings_vector <- planets_df$rings
+print(rings_vector)
+# use the rings vector just created to select the data for the four planets with rings
+planets_df[rings_vector, ]
+# should show us only planets that have rings  "[1] TRUE TRUE TRUE TRUE"
+
+
+
+
+
+
+
+
 
 
 
